d299b6bf008c45b50faa1dd03eee9955b4334e4e
neerajmoitra08 <neerajmoitra08@gmail.com>
2019-02-07 21:20:27 +0530

    Fragment

---
+++ app/src/main/java/info/andriodhive/hackathonuser/Threshold_Fragment.java
@@ -0,0 +1,29 @@
+package info.andriodhive.hackathonuser;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Threshold_Fragment extends Fragment {
+
+    public Threshold_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.threshold, container, false);
+    }
+}
